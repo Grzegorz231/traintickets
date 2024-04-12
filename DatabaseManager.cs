36d317ef195e95c58698cd17ecab7f35180b2ea2
Grzegorz231 <vova.hazhiev2014@yandex.ru>
2024-04-12 20:05:34 +0500

    добавлена авторизация системного администратора

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -9,21 +9,21 @@ namespace RailwayTickets
 {
     public class DatabaseManager
     {
-        private NpgsqlConnection _connection;
-        private string connectionString;
+        public NpgsqlConnection connection;
+        public string connectionString;
 
         public DatabaseManager(string server, int port, string database, string userId, string password)
         {
             connectionString = $"Server={server};Port={port};Database={database};User Id={userId};Password={password};";
-            _connection = new NpgsqlConnection(connectionString);
+            connection = new NpgsqlConnection(connectionString);
         }
         public void OpenConnection()
         {
-            _connection.Open();
+            connection.Open();
         }
         public void CloseConnection() 
         { 
-            _connection.Close(); 
+            connection.Close(); 
         }
     }
 }
--- SysAdminEnter.xaml.cs
+++ SysAdminEnter.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,7 @@ namespace RailwayTickets
 {
     public partial class SysAdminEnter : Window
     {
+        DatabaseManager databaseManager = new DatabaseManager("localhost", 5432, "trains", "postgres", "2514");
         public SysAdminEnter()
         {
             InitializeComponent();
@@ -23,7 +25,42 @@ namespace RailwayTickets
 
         private void btnSysAdmOK_Click(object sender, RoutedEventArgs e)
         {
+            databaseManager.OpenConnection();
+            NpgsqlCommand commandToCheckAdmin = new NpgsqlCommand(@"SELECT employee_post, employee_login, employee_password
+                                                                    FROM public.employee
+                                                                    WHERE employee_post = 'Системный администратор' AND employee_login = @login", databaseManager.connection);    
+            string login = txtBoxSysAdmLogin.Text;
+            string password = txtBoxSysAdmPassword.Text;
 
+            commandToCheckAdmin.Parameters.AddWithValue("@login", login);
+
+            NpgsqlDataReader reader = commandToCheckAdmin.ExecuteReader();
+
+            if (reader.Read())
+            {
+                string dbLogin, dbPassword;
+                dbLogin = reader["employee_login"].ToString();
+                dbPassword = reader["employee_password"].ToString();
+
+                if(login == dbLogin && password == dbPassword)
+                {
+                    this.Close();
+                    Registration registration = new Registration();
+                    registration.Owner = this.Owner;
+                    this.Owner.IsEnabled = false;
+                    registration.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }         
+            }
+            else
+            {
+                MessageBox.Show("Пользователь не найден!");
+            }
+            reader.Close();
+            databaseManager.CloseConnection();
         }
 
         private void btnSysAdmCancel_Click(object sender, RoutedEventArgs e)
